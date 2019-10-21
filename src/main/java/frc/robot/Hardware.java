88887c1e3a835cab5b50083ec3e86f2090904981
Patrick Henegan <phenegan03@gmail.com>
2019-10-21 15:53:24 -0400

    Base project

---
+++ src/main/java/frc/robot/Hardware.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import org.montclairrobotics.alloy.frc.FRCMotor;
+
+public class Hardware {
+
+    public static void init() {
+
+    }
+
+
+}
