ba9e569093f1a5376f22b10b154a9f0e0ea50cac
Patrick Henegan <phenegan03@gmail.com>
2019-10-21 21:40:16 -0400

    Did the below commit
    Didn't realize I never saved the file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,10 +7,15 @@
 
 package frc.robot;
 
-import org.montclairrobotics.alloy.drive.DriveModule;
+import org.montclairrobotics.alloy.drive.DriveTrain;
+import org.montclairrobotics.alloy.drive.MecanumDrive;
 import org.montclairrobotics.alloy.frc.FRCAlloy;
+import org.montclairrobotics.alloy.frc.FRCEncoder;
 import org.montclairrobotics.alloy.frc.FRCMotor;
-import org.montclairrobotics.alloy.motor.MotorGroup;
+import org.montclairrobotics.alloy.motor.MotorModule;
+import org.montclairrobotics.alloy.utils.PID;
+import org.montclairrobotics.alloy.vector.Angle;
+import org.montclairrobotics.alloy.vector.Polar;
 import org.montclairrobotics.alloy.vector.XY;
 
 /**
@@ -21,20 +26,31 @@ import org.montclairrobotics.alloy.vector.XY;
  * project.
  */
 public class Robot extends FRCAlloy {
-  
-  //drive train motors
-  FRCMotor fr = new FRCMotor(Hardware.dt_fr);
-  FRCMotor fl = new FRCMotor(Hardware.dt_fl);
-  FRCMotor br = new FRCMotor(Hardware.dt_br);
-  FRCMotor bl = new FRCMotor(Hardware.dt_bl);
-  //DriveModule rightModule = new DriveModule();
-  //DriveModule leftModule = new DriveModule();
+
+  // drive train motors
+
+  DriveTrain driveTrain;
 
   public void robotSetup() {
-    Hardware.init();
   }
 
   public void initialization(){
+    Hardware.init();
+
+    driveTrain = new MecanumDrive(
+      new MotorModule(
+        new Polar(-1, new Angle(-45)), new XY(-1, 1), new FRCEncoder(Hardware.dt_fl), new PID(0,0,0), new FRCMotor(Hardware.dt_fl) 
+      ),
+      new MotorModule(
+        new Polar(1, new Angle(45)), new XY(1, 1), new FRCEncoder(Hardware.dt_fr), new PID(0,0,0), new FRCMotor(Hardware.dt_fr)
+      ),
+      new MotorModule(
+        new Polar(-1, new Angle(45)), new XY(-1,-1), new FRCEncoder(Hardware.dt_bl), new PID(0,0,0), new FRCMotor(Hardware.dt_bl)
+      ),
+      new MotorModule(
+        new Polar(1, new Angle(-45)), new XY(1, -1), new FRCEncoder(Hardware.dt_br), new PID(0,0,0), new FRCMotor(Hardware.dt_br)
+      )  
+    );
 
   }
 
