f16dcde47c5fb2bdf63c241690efdfa0546f52df
Patrick Henegan <phenegan03@gmail.com>
2019-10-30 15:38:00 -0400

    Testing right lift motor
    implemented test code for the right motor

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -12,19 +12,29 @@ public class Hardware {
     public static WPI_TalonSRX dt_br;
     public static WPI_TalonSRX dt_bl;
 
-    //DriveTrain motor ids
+
+    public static WPI_TalonSRX rIntake;
+    public static WPI_TalonSRX lIntake;
+
+
+    //Motor port numbers
     public final static int DT_FRONT_LEFT = 2;
     public final static int DT_FRONT_RIGHT = 3;
     public final static int DT_BACK_LEFT = 1;
     public final static int DT_BACK_RIGHT = 0;
-    
+    public final static int INTAKE_RIGHT = 4;
+    public final static int INTAKE_LEFT = 5;
+
 
     public static void init() {
         dt_fr = new WPI_TalonSRX(DT_FRONT_RIGHT);
         dt_fl = new WPI_TalonSRX(DT_FRONT_LEFT);
         dt_br = new WPI_TalonSRX(DT_BACK_RIGHT);
         dt_bl = new WPI_TalonSRX(DT_BACK_LEFT);
-        }
+
+        rIntake = new WPI_TalonSRX(INTAKE_RIGHT);
+        lIntake = new WPI_TalonSRX(INTAKE_LEFT);
+    }
 
 
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,14 +9,14 @@ package frc.robot;
 
 import org.montclairrobotics.alloy.drive.DriveTrain;
 import org.montclairrobotics.alloy.drive.MecanumDrive;
-import org.montclairrobotics.alloy.frc.FRCAlloy;
-import org.montclairrobotics.alloy.frc.FRCEncoder;
-import org.montclairrobotics.alloy.frc.FRCMotor;
+import org.montclairrobotics.alloy.frc.*;
 import org.montclairrobotics.alloy.motor.MotorModule;
 import org.montclairrobotics.alloy.utils.PID;
 import org.montclairrobotics.alloy.vector.Angle;
 import org.montclairrobotics.alloy.vector.Polar;
 import org.montclairrobotics.alloy.vector.XY;
+
+import java.awt.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,36 +27,47 @@ import org.montclairrobotics.alloy.vector.XY;
  */
 public class Robot extends FRCAlloy {
 
-  DriveTrain driveTrain;
+    DriveTrain driveTrain;
 
-  public void robotSetup() {
-  }
+    FRCButton button;
+    FRCMotor motor;
 
-  public void initialization() {
-    Hardware.init();
-    //All this is is an instantiation with a lot of stuff it in: Object name = new Object(parameters);
-    //where the parameters also have new objects inside of them
-    driveTrain = new MecanumDrive(
-            new MotorModule(
-                    new Polar(1, new Angle(45)), new XY(-1, 1),
-                    new FRCEncoder(Hardware.dt_fl), new PID(0, 0, 0), new FRCMotor(Hardware.dt_fl)
-            ),
-            new MotorModule(
-                    new Polar(-1, new Angle(-45)), new XY(1, 1),
-                    new FRCEncoder(Hardware.dt_fr), new PID(0, 0, 0), new FRCMotor(Hardware.dt_fr)
-            ),
-            new MotorModule(
-                    new Polar(1, new Angle(45)), new XY(-1, -1),
-                    new FRCEncoder(Hardware.dt_bl), new PID(0, 0, 0), new FRCMotor(Hardware.dt_bl)
-            ),
-            new MotorModule(
-                    new Polar(-1, new Angle(-45)), new XY(1, -1),
-                    new FRCEncoder(Hardware.dt_br), new PID(0, 0, 0), new FRCMotor(Hardware.dt_br)
-            )
-    );
-  }
+    public void robotSetup() {
+        motor.enable();
+    }
+    public void initialization() {
+        Hardware.init();
 
-  public void periodic() {
+        button = new FRCButton( new FRCJoystick(1), 1);
+        motor = new FRCMotor(Hardware.rIntake);
 
-  }
+        //All this is is an instantiation with a lot of stuff it in: Object name = new Object(parameters);
+        //where the parameters also have new objects inside of them
+
+        /*
+        driveTrFRCButtonain = new MecanumDrive(
+                new MotorModule(
+                        new Polar(1, new Angle(45)), new XY(-1, 1),
+                        new FRCEncoder(Hardware.dt_fl), new PID(0, 0, 0), new FRCMotor(Hardware.dt_fl)
+                ),
+                new MotorModule(
+                        new Polar(-1, new Angle(-45)), new XY(1, 1),
+                        new FRCEncoder(Hardware.dt_fr), new PID(0, 0, 0), new FRCMotor(Hardware.dt_fr)
+                ),
+                new MotorModule(
+                        new Polar(1, new Angle(45)), new XY(-1, -1),
+                        new FRCEncoder(Hardware.dt_bl), new PID(0, 0, 0), new FRCMotor(Hardware.dt_bl)
+                ),
+                new MotorModule(
+                        new Polar(-1, new Angle(-45)), new XY(1, -1),
+                        new FRCEncoder(Hardware.dt_br), new PID(0, 0, 0), new FRCMotor(Hardware.dt_br)
+                )
+        ); */
+    }
+
+    public void periodic() {
+        if (button.getValue()) {
+            motor.setMotorPower(0.5);
+        }
+    }
 }
