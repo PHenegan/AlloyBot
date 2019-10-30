520f23132439a2396c059420cd6eba51da800e71
Patrick Henegan <phenegan03@gmail.com>
2019-10-30 18:27:37 -0400

    Modified Control.java
    Added placeholder values for some button IDs, and created objects for some buttons and joysticks

--- src/main/java/frc/robot/Control.java
+++ src/main/java/frc/robot/Control.java
@@ -1,5 +1,32 @@
 package frc.robot;
 
+import org.montclairrobotics.alloy.frc.FRCButton;
+import org.montclairrobotics.alloy.frc.FRCJoystick;
+
 public class Control {
-    //Inputs and controls go here
+    //IDs  -- NOT REAL VALUES!
+    public final static int BUTTON_ID_CIRCLE = 0;
+    public final static int BUTTON_ID_SQUARE = 1;
+    public final static int BUTTON_ID_X = 2;
+    public final static int BUTTON_ID_TRIANGLE = 3;
+
+    public final static int BUTTON_ID_START = 4;
+    public final static int BUTTON_ID_SELECT = 5;
+
+
+
+    //Buttons
+     public static FRCButton intake_down;
+     public static FRCButton intake_up;
+     public static FRCButton hatch;
+
+     //Joysticks
+     public static FRCJoystick drive_stick_0;
+     public static FRCJoystick drive_stick_1;
+
+     //Initialize all of the above objects here
+     public static void init() {
+
+     }
+
 }
